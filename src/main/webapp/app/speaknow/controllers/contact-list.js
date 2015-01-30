a78341b425d72c2b2dbfd9f70e02dd1619a84eb9
Vinicius da Costa Pires <vinicius.costa.pires@gmail.com>
2015-01-30 20:53:17 +0000

    Nova estrutura de módulos, adição de um script gerador de módulo, e início da configuração do ng-translate

--- src/main/webapp/app/speaknow/controllers/contact-list.js
+++
@@ -1,9 +0,0 @@
-define([
-    'connecta.speaknow'
-], function(speaknow) {
-    
-    return speaknow.lazy.controller('ContactListController', function($scope){
-        $scope.hello = "Hello SpeakNooooow";
-    });
-});
-

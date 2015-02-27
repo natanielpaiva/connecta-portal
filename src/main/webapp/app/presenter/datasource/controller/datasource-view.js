c0fb6bfd3c9e8390d5a54ae1284cf26dd87719eb
diego.rego <diegofernandesrego@users.noreply.github.com>
2015-02-27 14:34:20 +0000

    Correçao de no código

--- src/main/webapp/app/presenter/datasource/controller/datasource-view.js
+++ src/main/webapp/app/presenter/datasource/controller/datasource-view.js
@@ -2,12 +2,19 @@ define([
     'connecta.presenter',
     'presenter/datasource/service/datasource-service'
 ], function (presenter) {
-    return presenter.lazy.controller('DatasourceViewController', function ($scope, DatasourceService, $routeParams) {
+    return presenter.lazy.controller('DatasourceViewController', function ($scope, DatasourceService, $routeParams, $location) {
 
         DatasourceService.getById($routeParams.id).then(function (response) {
 
             $scope.datasource = response.data;
-
+            
+            $scope.excluir = function (id) {
+            DatasourceService.excluir(id).then(function (response) {
+                     $location.path('presenter/datasource');
+            }, function (response) {
+                
+            });
+        };
         });
 
     });

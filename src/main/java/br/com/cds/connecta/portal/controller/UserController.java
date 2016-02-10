b493c4108c972dfec07088a561a1118e077bc2af
Vinicius da Costa Pires <vinicius.costa.pires@gmail.com>
2016-02-10 17:53:13 +0000

    Configurações do OAuth2

--- src/main/java/br/com/cds/connecta/portal/controller/UserController.java
+++ src/main/java/br/com/cds/connecta/portal/controller/UserController.java
@@ -10,7 +10,6 @@ import br.com.cds.connecta.portal.business.applicationService.IAuthenticationAS;
 import br.com.cds.connecta.portal.business.applicationService.IUserAS;
 import br.com.cds.connecta.portal.domain.security.UserCredentialsDTO;
 import br.com.cds.connecta.portal.domain.security.UserDTO;
-import br.com.cds.connecta.portal.entity.UserImage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -33,9 +32,14 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("user")
 public class UserController {
 
-    @Autowired IUserAS userAS;
-    @Autowired IAuthenticationAS authAS;
-    @Autowired HibernateAwareObjectMapper objectMapper;
+    @Autowired
+    private IUserAS userAS;
+    
+    @Autowired
+    private IAuthenticationAS authAS;
+    
+    @Autowired
+    private HibernateAwareObjectMapper objectMapper;
 
     @PublicResource
     @RequestMapping(method = RequestMethod.POST)
@@ -95,9 +99,9 @@ public class UserController {
     @RequestMapping(value = "{username}/avatar", method = RequestMethod.GET, 
             produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
     public ResponseEntity getUserAvatar(@PathVariable("username") String username){
-        UserImage image = userAS.getUserImage(username);
+        byte[] image = userAS.getUserImage(username);
         if (Util.isNotNull(image)) {
-            return new ResponseEntity(image.getImage(), HttpStatus.OK);
+            return new ResponseEntity(image, HttpStatus.OK);
         } else {
             return new ResponseEntity(HttpStatus.NO_CONTENT);
         }
--- src/main/java/br/com/cds/connecta/portal/entity/User.java
+++ src/main/java/br/com/cds/connecta/portal/entity/User.java
@@ -1,15 +1,14 @@
 package br.com.cds.connecta.portal.entity;
 
 import br.com.cds.connecta.framework.core.entity.AbstractBaseEntity;
-import javax.persistence.CascadeType;
+import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.Lob;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -28,15 +27,25 @@ public class User extends AbstractBaseEntity {
     @Column(name = "PK_USER")
     private Long id;
 
-    @Column(name = "CD_LOGIN")
+    @Column(name = "DS_LOGIN")
     private String login;
+    
+    @Column(name = "DS_PASSWORD")
+    private String password;
 
     @Column(name = "URL_IMAGE")
     private String imageUrl;
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "FK_USER_IMG", referencedColumnName = "PK_USER_IMG")
-    private UserImage image;
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    @Column(name = "BN_IMAGE")
+    private byte[] image;
+    
+    @Column(name = "NB_FACEBOOK_ID")
+    private Long facebookId;
+    
+    @Column(name = "DS_GOOGLE_TOKEN")
+    private String googleToken;
 
     @Override
     public Long getId() {
@@ -55,12 +64,12 @@ public class User extends AbstractBaseEntity {
         this.login = login;
     }
 
-    public UserImage getImage() {
-        return image;
+    public String getPassword() {
+        return password;
     }
 
-    public void setImage(UserImage image) {
-        this.image = image;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getImageUrl() {
@@ -69,6 +78,30 @@ public class User extends AbstractBaseEntity {
 
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public Long getFacebookId() {
+        return facebookId;
+    }
+
+    public void setFacebookId(Long facebookId) {
+        this.facebookId = facebookId;
+    }
+
+    public String getGoogleToken() {
+        return googleToken;
+    }
+
+    public void setGoogleToken(String googleToken) {
+        this.googleToken = googleToken;
     }
 
 }

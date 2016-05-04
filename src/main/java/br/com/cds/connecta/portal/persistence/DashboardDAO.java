cd7b7f68cb03a97644349169e3f394dbb89cb953
Cleto Gadêlha <cletogadelha@users.noreply.github.com>
2016-05-04 14:41:23 +0000

    ajuste na busca de dashboard por dominio

--- src/main/java/br/com/cds/connecta/portal/persistence/DashboardDAO.java
+++ src/main/java/br/com/cds/connecta/portal/persistence/DashboardDAO.java
@@ -13,17 +13,17 @@ import org.springframework.data.repository.query.Param;
 public interface DashboardDAO extends JpaRepository<Dashboard, Long> {
 
     @Query("SELECT d FROM Dashboard d LEFT JOIN FETCH d.sections s WHERE d.id = :id "
-    		+ "AND UPPER(d.domain) = :domain")
+    		+ "AND UPPER(d.domain) = UPPER(:domain)")
     Dashboard findOneWithSections(@Param("id") Long id,@Param("domain") String domain);
     
     @Query("FROM Dashboard d WHERE TRANSLATE(TRIM(UPPER(d.name)),"
     		+ "'ÁÂÀÃÄÉÈÊẼËÍÌÎĨÏÓÒÔÕÖÚÙÛŨÜÇ','AAAAAEEEEEIIIIIOOOOOUUUUUC') LIKE :name")
     Page<Dashboard> findAllLikeName(@Param("name") String name, Pageable pageable);
     
-    @Query("FROM Dashboard d WHERE UPPER(d.domain) = :domain")
+    @Query("FROM Dashboard d WHERE UPPER(d.domain) = UPPER(:domain)")
     Page<Dashboard> findByDomain(@Param("domain") String domain, Pageable pageable);
     
-    @Query("FROM Dashboard d WHERE UPPER(d.domain) = :domain AND TRANSLATE(TRIM(UPPER(d.name)),"
+    @Query("FROM Dashboard d WHERE UPPER(d.domain) = UPPER(:domain) AND TRANSLATE(TRIM(UPPER(d.name)),"
     		+ "'ÁÂÀÃÄÉÈÊẼËÍÌÎĨÏÓÒÔÕÖÚÙÛŨÜÇ','AAAAAEEEEEIIIIIOOOOOUUUUUC') LIKE :name")
     Page<Dashboard> findByDomainLikeName(@Param("domain") String domain, 
     							@Param("name") String name, Pageable pageable);
